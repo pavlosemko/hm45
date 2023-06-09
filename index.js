9347ad43d0fbaa51dcdbfd26a5bc2d159a0ab669
Pavlo Semko <pavelsssemko@gmail.com>
2023-06-09 15:26:28 +0300

    Update index.js
    hm

--- index.js
+++ index.js
@@ -1,38 +1,17 @@
-class InputStorage{
-
-}
-const storage = new InputStorage();
-
-
-// це не подобалось бо була кома вкінці
-const _reWriteTextarea = function () {
-    const inputs = document.querySelectorAll(".input"),
-        textArea = document.getElementById("textarea");
-    let updatedValue = "";
-    inputs.forEach((input) => {
-        const id = input.id,
-            value = input.value;
-        if (value.length == 0 || storage[id] === value) return;
-        updatedValue += `${value},`;
-        storage[id] = value;
-    });
-    textArea.value += updatedValue;
-};
-
+const storage = {};
+ const inputs = document.querySelectorAll(".input"),
+        textArea = document.getElementById("textarea";
+                                           
 const reWriteTextarea = function () {
-    const inputs = document.querySelectorAll(".input"),
-        textArea = document.getElementById("textarea"),
-        textAreaList = textArea.value.split(","),
-        updatedValue = textAreaList.filter((e) => e !== "");
     inputs.forEach((input) => {
         const id = input.id,
             value = input.value;
         if (value.length == 0 || storage[id] === value) return;
-        updatedValue.push(value);
+        textArea.value += `${value},`;
         storage[id] = value;
     });
-    textArea.value = updatedValue.join(",");
 };
+
 reWriteTextarea();
 
 setInterval(reWriteTextarea, 4000);
