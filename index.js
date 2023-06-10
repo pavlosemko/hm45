b10cf07a2d3b15c07f5dfbe84efb6227b3684036
Pavlo <pavelsssemko@gmail.com>
2023-06-10 18:15:13 +0300

    fix hm

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const storage = {};
 const inputs = document.querySelectorAll(".input"),
-      textArea = document.getElementById("textarea";
-                                           
+    textArea = document.getElementById("textarea");
+
 const reWriteTextarea = function () {
+    let update = "";
     inputs.forEach((input) => {
         const id = input.id,
             value = input.value;
         if (value.length == 0 || storage[id] === value) return;
-        textArea.value += `${value},`;
+        update += `${value},`;
         storage[id] = value;
     });
+    if (update.length > 0) {
+        textArea.value += update;
+    }
 };
 
 reWriteTextarea();
